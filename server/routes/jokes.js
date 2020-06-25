e038c5976dcfd15f254f247da7976dcec2ab3c5e
Ariel <33848430+arielwc@users.noreply.github.com>
2020-06-26 10:38:38 +1200

    joke routes created

---
+++ server/routes/jokes.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const request = require('superagent')
+
+const router = express.Router()
+const jokesURL = 'https://official-joke-api.appspot.com/random_joke'
+
+router.get('/setup', (req, res) => {
+  request.get(jokesURL)
+    .then(response => {
+      const { joke } = response.body
+      res.json(response.body.setup)
+    })
+    .catch(err => {
+      res.status(500).send('ERROR:' + err.message)
+    })
+})
+
+router.get('/punchline', (req, res) => {
+  request.get(jokesURL)
+    .then(response => {
+      const { joke } = response.body
+      res.json(response.body.punchline)
+    })
+    .catch(err => {
+      res.status(500).send('ERROR:' + err.message)
+    })
+})
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 const path = require('path')
 const express = require('express')
 
-const server = express()
+const express = require('express')
+const path = require('path')
 
+
+const root = require('./routes/routes')
+const  = require('./routes/jokes')
+
+const server = express()
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
+
+
+server.use('/api/setup', setup)
+server.use('/api/punchline', punchline)
 
 module.exports = server
