52c3fc097ffbd4e5286bf9a7f5e1f1113a8fe795
Ariel <33848430+arielwc@users.noreply.github.com>
2020-06-26 11:40:11 +1200

    fixed git issues

--- server/routes/jokes.js
+++ server/routes/jokes.js
@@ -4,11 +4,10 @@ const request = require('superagent')
 const router = express.Router()
 const jokesURL = 'https://official-joke-api.appspot.com/random_joke'
 
-router.get('/jokes', (req, res) => {
+router.get('/', (req, res) => {
   request.get(jokesURL)
     .then(response => {
-      const { joke } = response.body
-      res.json(response.body.setup)
+        res.json(response.body)
     })
     .catch(err => {
       res.status(500).send('ERROR:' + err.message)
